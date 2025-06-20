9d09615f279322680327241c4e4a636955335b7e
J10RH <jhonnyrh1008@gmail.com>
2025-06-19 22:35:13 -0500

    Update app2_neutron.py

--- app2_neutron.py
+++ app2_neutron.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-
+import os
 st.set_page_config(page_title="Predicción COSMOS", layout="wide")
 st.title("🌱 Predicción de COSMOS Volumetric Water Content (%)")
 
